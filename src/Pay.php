59134acbacec2ca32f92bdd315b301e1c61d36d0
jikeSen <569045356@qq.com>
2018-11-09 13:04:58 +0800

    添加强制返回的类型

---
+++ src/Pay.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2018/11/9
+ * Time: 13:04
+ */
+
+namespace jikesen\jkPay;
+
+
+class Pay
+{
+    public function test(): string
+    {
+        return "hello , this is jike_pay test demo";
+    }
+}
